5e6c5efa094c6d7be3991dc42a754925f647bd03
Benedek <szankabg@gmail.com>
2022-12-22 15:19:14 +0100

    looks decent

--- src/Scripts/Boid.gd
+++ src/Scripts/Boid.gd
@@ -1,46 +1,4 @@
 extends Node2D
 
-#variables
-
-#position
-var direction: Vector2
-var new_direction: Vector2
-
-var RADIUS: int = 100
-var separation: bool = true
-var alignment: bool = true
-var cohesion: bool = true
-
-func _ready():
-	position = Vector2(rand_range(0,400), rand_range(0,400))
-	direction = Vector2(rand_range(-1,1), rand_range(-1,1))
-	direction /= direction.length()
-	pass
-
-
-func update_direction(flock):
-	var avg_position: Vector2
-	var avg_direction: Vector2
-	var separation_vec: Vector2
-	for boid in flock: 
-		var distance = (boid.position - position).length()
-		if boid.position != position and distance <= RADIUS:
-			if cohesion:
-				avg_position += boid.position * distance
-			if alignment:
-				avg_direction += boid.direction * distance
-			if separation:
-				separation_vec += (position - boid.position) / distance	
-			new_direction = separation_vec + avg_position + avg_direction
-			new_direction /= new_direction.length()
-	pass
-
-func move():
-	direction = new_direction
-	position += direction	
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	#position += direction
-	pass
+var d: Vector2
+var nd: Vector2
--- src/Scripts/Main.gd
+++ src/Scripts/Main.gd
@@ -1,25 +1,78 @@
 extends Node2D
 
+var boidScene = preload("res://Scenes/Boid.tscn")
+var boids = []
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var boidsNum = 200
+var visual_range = 50
+var max_speed = 10
+var min_speed = 5
+var margin = 50 
+var turn_factor = 1
 
-
-# Called when the node enters the scene tree for the first time.
-
-var boid = preload("res://Scenes/Boid.tscn")
-var flock = []
+var separation_factor = 0.05
+var alignment_factor = 0.1
+var cohesion_factor = 0.05
 
 func _ready():
-	for i in range(0, 20):
-		flock.append(boid.instance())
-		add_child(flock[i])
+	randomize()
+	for i in range(0, boidsNum):
+		var instance = boidScene.instance()
+		instance.position = Vector2(rand_range(0, OS.window_size.x), rand_range(0, OS.window_size.y))
+		instance.d = Vector2(rand_range(0, 10) - 5, rand_range(0, 10) - 5)
+		boids.append(instance)
+		add_child(instance)
 	pass
 
-
 func _process(delta):
-	for boid in flock:
-		boid.update_direction(flock)
-		boid.move()
+	for i in range(0, boids.size()):
+		var boid = boids[i]
+		boid.nd = boid.d
+		var cohesion: Vector2
+		var separation: Vector2
+		var alignment: Vector2
+		
+		var neighbors = 0
+		for j in range(0, boids.size()):
+			if i != j and (boid.position - boids[j].position).length() < visual_range:
+				neighbors += 1
+				cohesion += boids[j].position
+				var a = boid.position - boids[j].position
+				var asd = 2
+				if a.x != 0:
+					if a.x > 0:
+						separation.x += max(1 / a.x, asd)
+					else:
+						separation.x += min(1 / a.x, -asd)
+				if a.y != 0:
+					if a.y > 0:
+						separation.y += max(1 / a.y, asd)
+					else:
+						separation.y += min(1 / a.y, -asd)
+				alignment += boids[j].d
+		
+		if neighbors != 0:
+			boid.nd += separation * separation_factor
+			boid.nd += (alignment / neighbors - boid.d) * alignment_factor
+			boid.nd += (cohesion  / neighbors - boid.position) * cohesion_factor
+		
+		if boid.position.x < margin:
+			boid.nd.x += turn_factor
+		if boid.position.x > OS.window_size.x - margin:
+			boid.nd.x -= turn_factor
+		if boid.position.y < margin:
+			boid.nd.y += turn_factor
+		if boid.position.y > OS.window_size.y - margin:
+			boid.nd.y -= turn_factor
+		
+		var speed = boid.nd.length()
+		if speed > max_speed:
+			boid.nd *= max_speed / speed 
+		if speed < min_speed:
+			boid.nd *= min_speed / speed 			
+		
+	for i in range(0, boids.size()):
+		boids[i].d = boids[i].nd
+		boids[i].rotation = boids[i].d.angle()
+		boids[i].position += boids[i].d
 	pass
